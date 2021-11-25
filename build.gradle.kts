11aae69e6fec939427f447d35af76a19f8a7a39d
Naton1 <28943608+Naton1@users.noreply.github.com>
2021-11-25 07:44:23 -0500

    Refactor logic into separate tribot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,78 +1,18 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.tribot.gradle.plugin.getTribotDirectory
-
-val baseDir = projectDir
-val outputDir = getTribotDirectory().resolve("bin")
+import org.tribot.gradle.plugin.TribotPlugin
 
 plugins {
     java
-    kotlin("jvm") version "1.4.10"
-    id("org.openjfx.javafxplugin") version "0.0.9"
+    kotlin("jvm") version "1.5.31"
 }
 
-allprojects {
-
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-    apply(plugin = "org.openjfx.javafxplugin")
-
-    java.sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
-    java.targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
-
+buildscript {
     repositories {
-        jcenter()
-        maven("https://gitlab.com/api/v4/projects/20741387/packages/maven") // Tribot Central
-        google()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenCentral()
+        maven("https://gitlab.com/api/v4/projects/20741387/packages/maven")
     }
-
     dependencies {
-        api(files("${baseDir.absolutePath}/allatori-annotations-7.5.jar"))
-        api("org.tribot:tribot-script-sdk:0.0.42")
-    }
-
-    sourceSets {
-        main {
-            java {
-                setSrcDirs(listOf("src"))
-            }
-            resources {
-                setSrcDirs(listOf("src/resources"))
-            }
-            output.resourcesDir = outputDir
-            java.outputDir = outputDir
-        }
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-        destinationDir = outputDir
-    }
-
-    configurations.all {
-        // Ensures that our dependencies will update timely
-        resolutionStrategy.cacheDynamicVersionsFor(5, "minutes")
-        resolutionStrategy.cacheChangingModulesFor(5, "minutes")
-    }
-
-    tasks["clean"].doFirst {
-        outputDir.listFiles()?.forEach { it.deleteRecursively() }
+        classpath("org.tribot:tribot-gradle-plugin:+")
     }
 }
 
-val repoCopy = tasks.create("repoCopy") {
-    group = "tribot"
-    doLast {
-        val repoDeployDir = this.project.projectDir.resolve("build/repo-deploy").also { it.mkdirs() }
-        allprojects
-                .filter { it.path != project.path }
-                .map { it.projectDir.resolve("build/repo-deploy") }
-                .filter { it.exists() }
-                .mapNotNull { it.listFiles()?.getOrNull(0) }
-                .forEach { it.copyTo(repoDeployDir.resolve(it.name), overwrite = true) }
-    }
-}
-
-tasks.build {
-    finalizedBy(repoCopy)
-}
+apply<TribotPlugin>()
--- scripts/build.gradle.kts
+++ scripts/build.gradle.kts
@@ -3,12 +3,3 @@ import org.tribot.gradle.plugin.TribotPlugin
 subprojects {
     apply<TribotPlugin>()
 }
-
-subprojects {
-    tasks {
-        getAt("repoPackage").dependsOn(assemble)
-        classes {
-            finalizedBy(getAt("repoPackage"))
-        }
-    }
-}
